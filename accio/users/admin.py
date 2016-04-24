8c062a5e6b92f2979e897fcb3e10c4cc14f47c3a
Rolf Erik Lekang <me@rolflekang.com>
2016-04-24 14:59:32 +0200

    fix: Remove domain from user admin

--- accio/users/admin.py
+++ accio/users/admin.py
@@ -8,8 +8,8 @@ from .models import User
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'domain')}),
+        (None, {'fields': ('username',)}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
