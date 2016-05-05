24ed97f09707a404bf81062a99a485c547d92d11
Rolf Erik Lekang <me@rolflekang.com>
2016-05-05 11:20:41 +0200

    fix: Add csrf_exempt to webhook view

--- accio/webhooks/views.py
+++ accio/webhooks/views.py
@@ -1,5 +1,7 @@
 from django.http.response import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def webhook(request):
     return HttpResponse('Webhooks not implemented yet', status=501)
